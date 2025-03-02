e43c65ca9e0bcb59a2fbb8c656a7d53fbf40cfce
Tt <tt.taiton@gmail.com>
2025-03-03 03:30:08 +0700

    add in function add

---
+++ src/lib/add.ts
@@ -0,0 +1,17 @@
+
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { db } from './firebase-config';
+import { UserData } from '@/lib/enum';
+
+export const addUser = async (userData: UserData) => {
+  try {
+    const docRef = await addDoc(collection(db, 'soul-users'), {
+      ...userData, 
+      createdAt: serverTimestamp() // Add timestamp here
+    });
+    console.log("User added successfully! with ID: ", docRef.id);
+  } catch (error) {
+    console.error('Error adding document:', error);
+  }
+};
+
