f9ed942adf6c337aee16917135a40929885c7b46
Zeeneemee <106436879+Zeeneemee@users.noreply.github.com>
2025-03-03 16:23:14 +0700

    add stressafter score into firebase (#60)

--- src/app/[locale]/(6)/6-3-4/page.tsx
+++ src/app/[locale]/(6)/6-3-4/page.tsx
@@ -3,48 +3,16 @@ import Bar from "@/components/ui/bar";
 import NextButton from "@/components/ui/NextButton";
 import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
-import React, { useState, useEffect } from "react";
-import { UserData } from "@/lib/enum";
-import { useLogicCalculation } from "@/lib/logicCalculation/logicCalculation";
+import React, { useState } from "react";
+
 
 const Scene6_3Page4: React.FC = () => {
   const t = useTranslations("6-3-4");
   const [feedbackScore, setFeedbackScore] = useState<number>(5);
   const [mouseDown, setMouseDown] = useState(false);
-  const [userData, setUserData] = useState<UserData | null>(null);
-  const { seasons, chapter } = useLogicCalculation();
 
-  useEffect(() => {
-    const name = String(localStorage.getItem("name"));
-    const feelings = Number(localStorage.getItem("stressScore"));
-    const result = { season: seasons, chapter: chapter };
-    const feedBack = String(localStorage.getItem("feedbackScore"));
-    setUserData({ name, feelings, result, feedBack });
-  }, [feedbackScore]);
 
-  const handleClick = async () => {
-    if (!userData) {
-      return;
-    }
-    try {
-      const response = await fetch("/api/add-user", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(userData),
-      });
 
-      const data = await response.json();
-
-      if (!response.ok) {
-        console.error("Error saving user data:", data.message);
-        return;
-      }
-
-      console.log("User data added successfully:", data);
-    } catch (error) {
-      console.error("Network error:", error);
-    }
-  };
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
       <motion.div
@@ -83,7 +51,7 @@ const Scene6_3Page4: React.FC = () => {
         exit={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5, duration: 1 }}
       >
-        <NextButton url="6-3-5" disabled={!mouseDown} onClick={handleClick} />
+        <NextButton url="6-3-5" disabled={!mouseDown} />
       </motion.div>
     </div>
   );
--- src/app/[locale]/(6)/6-3-5/page.tsx
+++ src/app/[locale]/(6)/6-3-5/page.tsx
@@ -4,12 +4,54 @@ import NextButton from "@/components/ui/NextButton";
 import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 import React, { useState } from "react";
+import { useEffect } from "react";
+import { UserData } from "@/lib/enum";
+import { useLogicCalculation } from "@/lib/logicCalculation/logicCalculation";
 
 const Scene6_3Page4: React.FC = () => {
   const t = useTranslations("6-3-5");
   const [stressAfterScore, setstressAfterScore] = useState<number>(5);
   const [mouseDown, setMouseDown] = useState(false);
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const { seasons, chapter } = useLogicCalculation();
+  
+  useEffect(() => {
+    const name = String(localStorage.getItem("name")) ;
+    const feelings = Number(localStorage.getItem("stressScore")) ;
+    const result =  {"season": seasons, "chapter": chapter} ;
+    const feedBack = String(localStorage.getItem("feedbackScore")) ;
+    const feelingsAfter = Number(localStorage.getItem("stressAfterScore")) ;
+  
+    setUserData({ name, feelings, result, feedBack, feelingsAfter });
+    console.log("userData", userData);
+  }, [stressAfterScore]);
+  
+  
 
+  const handleClick = async () => {
+    if (!userData) {
+      return;
+    }
+    try {
+      const response = await fetch("/api/add-user", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(userData),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        console.error("Error saving user data:", data.message);
+        return;
+      }
+
+      console.log("User data added successfully:", data);
+    } catch (error) {
+      console.error("Network error:", error);
+    }
+  };
+  
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
       <motion.div
@@ -48,7 +90,7 @@ const Scene6_3Page4: React.FC = () => {
         exit={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5, duration: 1 }}
       >
-        <NextButton url="6-3-7" disabled={!mouseDown} />
+        <NextButton url="6-3-7" disabled={!mouseDown} onClick={handleClick} />
       </motion.div>
     </div>
   );
